282444d86e273824bff805081e3cdcfdfcda4373
Wootae Yang <56479033+wyang19a@users.noreply.github.com>
2021-08-04 19:21:48 -0400

    Adds dynamic clock, scaffolds input and button
    TODO: work on `addTodoHandler()`

--- force-app/main/default/lwc/toDoManager/toDoManager.js
+++ force-app/main/default/lwc/toDoManager/toDoManager.js
@@ -1,23 +1,41 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
+// Track is optional as of SP20 update.
+// it is still recommended to use it to listen to changes.
 
 export default class ToDoManager extends LightningElement {
-  time = '8:15 PM'
-  greeting = "Good Evening"
+  @track time = '8:15 PM' // @track OPTIONAL as of Sp20
+  @track greeting = "Good Evening" // @track OPTIONAL as of Sp20
 
+  //LifeCycle Method
+  connectedCallback() {
+    this.getTime();
+
+    setInterval(() => {
+      this.getTime()
+      console.log("set interval called")
+    }, 60000)
+  }
   getTime() {
     const date = new Date();
     const hour = date.getHours();
     const min = date.getMinutes();
+
+    this.time = `${this.getHour(hour)}:${this.getDoubleDigit(min)} ${this.getMidDay(hour)}`
+
+    this.setGreeting(hour);
   }
 
   getHour(hour) {
     return hour === 0 ? 12 : hour > 12 ? (hour - 12) : hour
   }
-  
+
   getMidDay(hour) {
     return hour >= 12 ? "PM" : "AM"
   }
 
+  getDoubleDigit(digit) {
+    return digit < 10 ? "0" + digit : digit
+  }
   setGreeting(hour) {
     if (hour < 12) {
       this.greeting = "Good Morning"
